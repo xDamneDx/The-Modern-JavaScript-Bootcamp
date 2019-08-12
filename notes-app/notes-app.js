18e4565dd52338bbebef961acc2cf9fedffe7933
Владимир Ливинский <xDamneDx@Gmail.com>
2019-08-12 21:50:10 +0300

    In the process of passing 'data storage libraries and more' chapter

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -21,20 +21,43 @@
 // newParagraph.textContent = 'This is a new element from JavaScript';
 // document.querySelector('body').appendChild(newParagraph);
 
-const notes = [{
-    title: 'My next trip',
-    body: 'I would like to go to Spain'
-}, {
-    title: 'Habbits to work on',
-    body: 'Exercise. Eating a bit better.'
-}, {
-    title: 'Office modification',
-    body: 'Get a new seat'
-}];
+// const notes = [{
+//     title: 'My next trip',
+//     body: 'I would like to go to Spain'
+// }, {
+//     title: 'Habbits to work on',
+//     body: 'Exercise. Eating a bit better.'
+// }, {
+//     title: 'Office modification',
+//     body: 'Get a new seat'
+// }];
+
+let notes = [];
 
 const filters = {
     searchText: ''
 }
+
+// Check for existing save data:
+
+const notesJSON = localStorage.getItem('notes');
+
+if (notesJSON !== null) {
+    notes = JSON.parse(notesJSON);
+}
+
+// const user = {
+//     name: 'Vladimir',
+//     age: 33
+// }
+
+// const userJSON = JSON.stringify(user);
+// console.log(userJSON);
+// localStorage.setItem('user', userJSON);
+
+// const userJSON = localStorage.getItem('user');
+// const user = JSON.parse(userJSON);
+// console.log(`${user.name} is ${user.age}`);
 
 const renderNotes = function (notes, filters) {
     const filteredNotes = notes.filter(function (note) {
@@ -45,7 +68,13 @@ const renderNotes = function (notes, filters) {
 
     filteredNotes.forEach(function (note) {
         const noteEl = document.createElement('p');
-        noteEl.textContent = note.title;
+
+        if (note.title.length > 0) {
+            noteEl.textContent = note.title;
+        } else {
+            noteEl.textContent = 'Unnamed note';
+        }
+        
         document.querySelector('#notes').appendChild(noteEl);
     });
 };
@@ -53,7 +82,14 @@ const renderNotes = function (notes, filters) {
 renderNotes(notes, filters);
 
 document.querySelector('#create-note').addEventListener('click', function () {
-    console.log('New note created');
+    notes.push({
+        title: '',
+        body: ''
+    });
+
+    localStorage.setItem('notes', JSON.stringify(notes));
+
+    renderNotes(notes, filters);
 });
 
 document.querySelector('#search-text').addEventListener('input', function (e) {
--- todos-app/todos-app.js
+++ todos-app/todos-app.js
@@ -178,29 +178,97 @@
 //     renderTodos(todos, filters);
 // });
 
-// Challenge 7:
+// // Challenge 7:
 
-const todos = [{
-    text: 'Wake up',
-    completed: true
-}, {
-    text: 'Have breakfast',
-    completed: true
-}, {
-    text: 'Walk the dog',
-    completed: true
-}, {
-    text: 'Feed the animals',
-    completed: false
-}, {
-    text: 'Learn something new',
-    completed: false
-}];
+// const todos = [{
+//     text: 'Wake up',
+//     completed: true
+// }, {
+//     text: 'Have breakfast',
+//     completed: true
+// }, {
+//     text: 'Walk the dog',
+//     completed: true
+// }, {
+//     text: 'Feed the animals',
+//     completed: false
+// }, {
+//     text: 'Learn something new',
+//     completed: false
+// }];
+
+// const filters = {
+//     searchText: '',
+//     hideCompleted: false
+// };
+
+// const renderTodos = function (array, filters) {
+//     const filteredTodos = array.filter(function (todo) {
+//         if (filters.hideCompleted) {
+//             return todo.text.toLowerCase().includes(filters.searchText.toLowerCase()) && !todo.completed;
+//         } else {
+//             return todo.text.toLowerCase().includes(filters.searchText.toLowerCase());
+//         }
+//     });
+
+//     const leftTodos = filteredTodos.filter(function (todo) {
+//         return !todo.completed;
+//     });
+
+//     document.querySelector('#todos').innerHTML = '';
+
+//     const summary = document.createElement('h3');
+//     summary.textContent = `You have ${leftTodos.length} todos left:`;
+//     document.querySelector('#todos').appendChild(summary);
+
+//     filteredTodos.forEach(function (todo) {
+//         const newParagraph = document.createElement('p');
+//         newParagraph.textContent = `- ${todo.text}`;
+//         document.querySelector('#todos').appendChild(newParagraph);
+//     });
+// };
+
+// renderTodos(todos, filters);
+
+// document.querySelector('#todo-search').addEventListener('input', function (evt) {
+//     filters.searchText = evt.target.value;
+
+//     renderTodos(todos, filters);
+// });
+
+// document.querySelector('#todo-form').addEventListener('submit', function (evt) {
+//     evt.preventDefault();
+
+//     todos.push({
+//         text: evt.target.elements.todoText.value,
+//         completed: false
+//     });
+
+//     evt.target.elements.todoText.value = '';
+
+//     renderTodos(todos, filters);
+// });
+
+// document.querySelector('#hide-completed').addEventListener('change', function (evt) {
+//     filters.hideCompleted = evt.target.checked;
+
+//     renderTodos(todos, filters);
+// });
+
+// Challenge 8:
+
+let todos = [];
 
 const filters = {
     searchText: '',
     hideCompleted: false
 };
+
+const todosJSON = localStorage.getItem('todos');
+
+if (todosJSON !== null) {
+    todos = JSON.parse(todosJSON);
+}
 
 const renderTodos = function (array, filters) {
     const filteredTodos = array.filter(function (todo) {
@@ -243,6 +311,8 @@ document.querySelector('#todo-form').addEventListener('submit', function (evt) {
         text: evt.target.elements.todoText.value,
         completed: false
     });
+
+    localStorage.setItem('todos', JSON.stringify(todos));
 
     evt.target.elements.todoText.value = '';
 
