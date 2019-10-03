5f3102b006b0f47ffce4d540b72fc3d98141d71f
Владимир Ливинский <xDamneDx@Gmail.com>
2019-10-03 20:14:56 +0300

    In the process of passing 'Advanced Objects and Functions' chapter

--- hangman/app.js
+++ hangman/app.js
@@ -1,1 +1,12 @@
- 
+// Primitive value: string, number, boolean, null, undefined
+
+// Object: myObject --> Object.prototype --> null
+// Array: myArray --> Array.prototype --> Object.prototype --> null
+// Function: myFunct --> Function.prototype --> Object.prototype --> null
+// Function: myString --> String.prototype --> Object.prototype --> null
+// Function: myNumber --> Number.prototype --> Object.prototype --> null
+// Function: myBoolean --> Boolean.prototype --> Object.prototype --> null
+
+// const product = 'Computer';
+const product = new String('Computer');
+console.log(product);
--- hangman/hangman.js
+++ hangman/hangman.js
@@ -1,7 +1,18 @@
 const Hangman = function (word, remainingGuesses) {
     this.word = word.toLowerCase().split('');
     this.remainingGuesses = remainingGuesses;
-    this.guesses = ['c', 'b', 't', 'e'];
+    this.guesses = [];
+};
+ 
+Hangman.prototype.makeGuess = function (guess) {
+    guess = guess.toLowerCase();
+
+    if (!this.guesses.includes(guess)) {
+        this.guesses += guess;
+        if (!this.word.includes(guess)) {
+            this.remainingGuesses--;
+        }
+    }
 };
 
 Hangman.prototype.getPuzzle = function () {
@@ -15,11 +26,16 @@ Hangman.prototype.getPuzzle = function () {
         }
     });
 
-    return result;
+    return result + `. Remaining guesses: ${this.remainingGuesses}`;
 };
 
 const firstGame = new Hangman('Cat', 2);
 const secondGame = new Hangman('New Jersey', 6);
 
+firstGame.makeGuess('c');
+firstGame.makeGuess('t');
+firstGame.makeGuess('z');
 console.log(firstGame.getPuzzle());
+
+secondGame.makeGuess('w');
 console.log(secondGame.getPuzzle());
