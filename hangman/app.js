06038c9b222c35d25525e7a66be99d89b10c02c0
Владимир Ливинский <xDamneDx@Gmail.com>
2019-10-10 17:56:12 +0300

    In the process of passing 'Asynchronous JavaScript' chapter

--- hangman/app.js
+++ hangman/app.js
@@ -1,3 +1,7 @@
+// HTTP (Hyper Text Transfer Protocol)
+// Request - what do we want to do
+// Respomse - what was actually done
+
 const puzzleEl = document.querySelector('#puzzle');
 const statusEl = document.querySelector('#status');
 const firstGame = new Hangman('New York', 4);
@@ -11,3 +15,39 @@ window.addEventListener('keypress', function (e) {
     puzzleEl.textContent = `Word: ${firstGame.puzzle}`;
     statusEl.textContent = firstGame.message;
 });
+
+// Making an HTTP request
+// HTTP Statuses: https://httpstatuses.com/
+
+// const request = new XMLHttpRequest();
+
+// request.addEventListener('readystatechange', (e) => {
+//     if (e.target.readyState === 4 && e.target.status === 200) {
+//         const data = JSON.parse(e.target.responseText);
+//         console.log(data);
+//     } else if (e.target.readyState === 4) {
+//         console.log('An error has taken place');
+//     }
+// });
+
+// request.open('GET', 'http://puzzle.mead.io/puzzle?wordCount=3');
+// request.send();
+
+// http://restcountries.eu/#api-endpoints-all
+// http://restcountries.eu/rest/v2/all
+
+const countryCode = 'UA';
+const countryRequest = new XMLHttpRequest;
+
+countryRequest.addEventListener('readystatechange', (e) => {
+    if (e.target.readyState === 4 && e.target.status === 200) {
+        const data = JSON.parse(e.target.responseText);
+        const country = data.find((country) => country.alpha2Code === countryCode);
+        console.log(country.name);
+    } else if (e.target.readyState === 4) {
+        console.log('Something wrong');
+    }
+});
+
+countryRequest.open('GET', 'http://restcountries.eu/rest/v2/all');
+countryRequest.send();
