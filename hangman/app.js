1396f0103a808a9b6673fe35d5c6e8472911e05b
Владимир Ливинский <xDamneDx@Gmail.com>
2019-10-11 17:27:27 +0300

    In the process of passing 'Asynchronous JavaScript' chapter

--- hangman/app.js
+++ hangman/app.js
@@ -16,22 +16,16 @@ window.addEventListener('keypress', function (e) {
     statusEl.textContent = firstGame.message;
 });
 
+getPuzzle((error, puzzle) => {
+    if (error) {
+        console.log(`Error: ${error}`);
+    } else {
+        console.log(puzzle);
+    }
+});
+
 // Making an HTTP request
 // HTTP Statuses: https://httpstatuses.com/
-
-// const request = new XMLHttpRequest();
-
-// request.addEventListener('readystatechange', (e) => {
-//     if (e.target.readyState === 4 && e.target.status === 200) {
-//         const data = JSON.parse(e.target.responseText);
-//         console.log(data);
-//     } else if (e.target.readyState === 4) {
-//         console.log('An error has taken place');
-//     }
-// });
-
-// request.open('GET', 'http://puzzle.mead.io/puzzle?wordCount=3');
-// request.send();
 
 // http://restcountries.eu/#api-endpoints-all
 // http://restcountries.eu/rest/v2/all
