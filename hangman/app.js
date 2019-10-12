36adf3ab5a518316f19f4339416a0c14dff1cd8f
Владимир Ливинский <xDamneDx@Gmail.com>
2019-10-12 16:50:01 +0300

    In the process of passing 'Asynchronous JavaScript' chapter

--- hangman/app.js
+++ hangman/app.js
@@ -31,17 +31,11 @@ getPuzzle((error, puzzle) => {
 // http://restcountries.eu/rest/v2/all
 
 const countryCode = 'UA';
-const countryRequest = new XMLHttpRequest;
 
-countryRequest.addEventListener('readystatechange', (e) => {
-    if (e.target.readyState === 4 && e.target.status === 200) {
-        const data = JSON.parse(e.target.responseText);
-        const country = data.find((country) => country.alpha2Code === countryCode);
-        console.log(country.name);
-    } else if (e.target.readyState === 4) {
-        console.log('Something wrong');
+getCountryDetails(countryCode, (error, country) => {
+    if (error) {
+        console.log(`Error: ${error}`);
+    } else {
+        console.log(`Country name: ${country.name}`);
     }
-});
-
-countryRequest.open('GET', 'http://restcountries.eu/rest/v2/all');
-countryRequest.send();
+})
--- hangman/requests.js
+++ hangman/requests.js
@@ -13,3 +13,20 @@ getPuzzle = (callback) => {
     request.open('GET', 'http://puzzle.mead.io/puzzle?wordCount=3');
     request.send();
 };
+
+getCountryDetails = (countryCode, callback) => {
+    const request = new XMLHttpRequest();
+
+    request.addEventListener('readystatechange', (e) => {
+        if (e.target.readyState === 4 && e.target.status === 200) {
+            const data = JSON.parse(e.target.responseText);
+            const country = data.find((country) => country.alpha2Code === countryCode);
+            callback(undefined, country);
+        } else if (e.target.readyState === 4) {
+            callback('Something goes wrong', undefined);
+        }
+    });
+
+    request.open('GET', 'http://restcountries.eu/rest/v2/all');
+    request.send();
+}
