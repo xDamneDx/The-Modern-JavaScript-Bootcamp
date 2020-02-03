1cf946048423313acc9e8a5d2dd76b6fc71c2517
Владимир Ливинский <xDamneDx@Gmail.com>
2020-02-03 13:10:22 +0200

    In the process of passing 'Cutting-Edge JavaScript with Babel and Webpack' chapter

---
+++ boilerplate/src/index.js
@@ -0,0 +1,6 @@
+import square, { add, name } from './utilities';
+import { scream } from './scream';
+
+console.log(add(10, 15)); // 25
+console.log(name); // Vladimir
+console.log(scream('aaaaa'));
---
+++ boilerplate/src/utilities.js
@@ -0,0 +1,8 @@
+// Named export
+// Default export
+
+export const add = (a, b) => a + b;
+export const name = 'Vladimir';
+
+const square = (x) => x * x;
+export default square;
